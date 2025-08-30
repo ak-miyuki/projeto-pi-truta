25006b6783c5da2f9395ac20ee1a5d6ee632e3c1
maria-maia <maria.maia@sptech.school>
2025-08-30 16:36:25 -0300

    adicionado insert e selects em todas as tabelas

--- Banco De Dados Grupo 10.sql
+++ Banco De Dados Grupo 10.sql
@@ -33,12 +33,18 @@ INSERT INTO cliente VALUES
 	(default,'Peixes Co','5513987610000','renoh3389','78.687.100/0001-25','2023-07-17','peixesco@gmail.com'),
 	(default,'Filhos da Truta','551892761100','vb3894','66.800.473/0001-81','2021-05-27','filhosdatruta@gmail.com'),
 	(default,'Kamehame Peixes','5511922224000','n4op8912','46.668.258/0001-61','2023-12-30','kamehamepeixes@gmail.com'),
-	(default,'Truta Olimpica','5511956619000','fbiup238','70.035.407/0001-30','2024-01-01','trutaolimpica@gmail.com');
+	(default,'Truta Olimpica','5511956619000','fbiup238','70.035.407/0001-30','2024-01-01','trutaolimpica@gmail.com'),
+	
+	(default,'Como pode um peixe','98572-7465','kdmjs874','86.852.658/0001-80','2025-08-03','peixevivo@outlook.com'),
+    (default,'Shark Powder','99325-7863','kfoei987c','52.687.852/0001-95','2025-06-19','sharkpowder@outlook.com')
+;
 
 SELECT * FROM cliente;
 
 SELECT concat(senha ,' ', email) AS login FROM cliente
 	WHERE dtCadastro > '2023-05-27' ORDER BY empresa DESC;
+
+SELECT empresa, cnpj FROM cliente ORDER BY empresa ASC;
     
 SELECT empresa AS 'Nome da empresa',
 	CASE
@@ -58,19 +64,26 @@ CREATE TABLE login (
     email VARCHAR(50) NOT NULL UNIQUE
 );
 
-/*
+
 insert into login values
-	(default,'Fish Company' ,'5511987614433' ,'huwiefh124' ,'10.245.633/0001-13' ,'fishcompany@gmail.com'),
-	(default,'Peixes Co' ,'5511987614433' ,'renoh3389' ,'78.687.100/0001-25' ,'peixesco@gmail.com'),
-	(default,'Filhos da Truta' ,'5511987614433' ,'vb3894' ,'66.800.473/0001-81' ,'filhosdatruta@gmail.com'),
-	(default,'Kamehame Peixes' ,'5511987614433' ,'n4op8912' ,'46.668.258/0001-61' ,'kamehamepeixes@gmail.com'),
-	(default,'Truta Olimpica' ,'5511987614433' ,'fbiup238' ,'70.035.407/0001-30', 'trutaolimpica@gmail.com');
+	(default,'Fish Company' ,'96817-8615' ,'huwiefh124' ,'10.245.633/0001-13' ,'fishcompany@gmail.com'),
+	(default,'Peixes Co' ,'96786-1662' ,'renoh3389' ,'78.687.100/0001-25' ,'peixesco@gmail.com'),
+	(default,'Filhos da Truta' ,'97659-7323' ,'vb3894' ,'66.800.473/0001-81' ,'filhosdatruta@gmail.com'),
+	(default,'Kamehame Peixes' ,'99256-1461' ,'n4op8912' ,'46.668.258/0001-61' ,'kamehamepeixes@gmail.com'),
+	(default,'Truta Olimpica' ,'99287-6462' ,'fbiup238' ,'70.035.407/0001-30', 'trutaolimpica@gmail.com'),
+    
+    (default,'Como pode um peixe','98572-7465','kdmjs874','86.852.658/0001-80','peixevivo@outlook.com'),
+    (default,'Shark Powder','99325-7863','kfoei987c','52.687.852/0001-95','sharkpowder@outlook.com')
+;
 
 
 SELECT concat(senha ,' ', email) AS login FROM cliente
 	WHERE dtCadastro > '2023-05-27' ORDER BY empresa DESC;
     
-*/
+
+
+SELECT email FROM login 
+	WHERE email ='gmail';
 
 CREATE TABLE coletaArduino (
     idSensor INT PRIMARY KEY AUTO_INCREMENT,
@@ -87,8 +100,10 @@ INSERT INTO coletaArduino VALUES
 	(default,'Peixes Co', 1,'2025-07-10 13:40:59', 19.5),
 	(default,'Filhos da Truta', 1,'2024-08-10 17:50:33', 15.3),
 	(default,'Kamehame Peixes', 1,'2023-01-15 22:22:16', 18.9),
-	(default,'Truta Olimpica', 1,'2021-08-30 23:10:31', 16);
-
+	(default,'Truta Olimpica', 1,'2021-08-30 23:10:31', 16),
+    (default,'Como pode um peixe', 1,'2025-08-03 15:10:12', 8),
+	(default,'Shark Powder', 1,'2025-06-19 15:10:12', 6.3);
+    
 SELECT * FROM coletaArduino;
 
 SELECT CONCAT(tanque ,' ', temperatura) AS medidor FROM coletaArduino
@@ -99,6 +114,11 @@ SELECT empresa AS 'Nome da empresa',
 		CONCAT('ATENÇÃO!!! A TEMPERATURA DO TANQUE NÚMERO ', tanque, ' ESTÁ ACIMA DO IDEAL (', temperatura, '°C).') AS Aviso
         FROM coletaArduino
         WHERE temperatura > 20;
+        -- empresa: tanque X precisa de atenção. temperatura em x graus
+SELECT tanque AS 'Número do tanque',
+	CONCAT(empresa,': O tanque ',tanque,' precisa de atenção. Temperatura em ',temperatura,' graus.') AS aviso
+    FROM coletaArduino
+    WHERE temperatura <10 OR temperatura>20;
     
 CREATE TABLE tanque (
 	idTanque INT PRIMARY KEY AUTO_INCREMENT,
@@ -122,7 +142,10 @@ INSERT INTO tanque VALUES
 	(default, 'Peixes Co', 1, 19.5, 10, 15, 20, 30, 'cultivo', false),
 	(default, 'Filhos da Truta', 1, 15.3, 10, 15, 20, 50, 'cultivo', true),
 	(default, 'Kamehame Peixes', 1, 18.9, 15, 16, 17, 300, 'crescimento', false),
-	(default, 'Truta Olimpica', 1, 16, 15, 16, 17, 400, 'crescimento', false);
+	(default, 'Truta Olimpica', 1, 16, 15, 16, 17, 400, 'crescimento', false),
+	(default, 'Como pode um peixe', 1, 18.3, 10, 15, 20, 750, 'cultivo', true),
+	(default, 'Shark Powder', 1, 17.6, 15, 16, 17, 83, 'crescimento', false)
+    ;
     
     SELECT * FROM tanque;
     
@@ -137,3 +160,10 @@ INSERT INTO tanque VALUES
             FROM tanque
             WHERE periodoFertil = true
             ORDER BY empresa;
+            
+SELECT empresa, tanque, faseTruta as 'Fase',
+ CASE
+ WHEN periodoFertil = 'true' THEN 'Seus peixes estão em período fértil'
+ ELSE 'Seus peixes não estão em período fértil'
+ END AS 'Período fértil'
+ FROM tanque ORDER BY periodoFertil ASC;
